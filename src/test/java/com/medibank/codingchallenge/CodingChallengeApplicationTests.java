97a50bfc4e2f2422909d3870e4b00ea679ddab3d
Karuna <karuna.joshi.oz@gmail.com>
2021-08-26 13:30:17 +1000

    Add files via upload

---
+++ src/test/java/com/medibank/codingchallenge/CodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibank.codingchallenge;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengeApplicationTests {
+
+	
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
